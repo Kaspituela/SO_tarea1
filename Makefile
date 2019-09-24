d47c4af88679a4b2111830fb5e919087d4fb0e02
Guillermo Vásquez <guillermo.vasquezh@sansano.usm.cl>
2019-09-24 16:37:40 -0300

    Parte 1 lista, reparte las cartas a los jugadores y deja la ultima carta

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=gcc
-CFLAGS=-I.
+CFLAGS=-I. -g
 
 tarea: tarea.c
 	$(CC) -o tarea tarea.c $(CFLAGS)
--- tarea.c
+++ tarea.c
@@ -2,8 +2,92 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
+#include <time.h>
+
+void eliminarCartas(char *jugador){
+  char s[100];
+  printf("%s\n",jugador);
+  printf("%s\n", getcwd(s, 100));
+  DIR *dir;
+  struct dirent *file;
+  dir = opendir(jugador);
+  chdir(jugador);
+  if (dir) {
+    while ((file = readdir(dir)) != NULL) {
+      if ( strcmp(file->d_name,".") != 0 && strcmp(file->d_name,"..") !=0 ){
+        remove(file->d_name);
+        printf("%s\n", file->d_name);
+      }
+    }
+  }
+  chdir("..");
+  closedir(dir);
+}
+void entregarCarta(char *destino, char *carta){
+    chdir(destino);
+    FILE *fp;
+    fp = fopen(carta,"w");
+    fclose(fp);
+    chdir("..");
+}
+
+char* obtenerCarta(int cant){
+  char s[100];
+  int i = 1;
+  char *carta = (char *)malloc(sizeof(char)*15);
+  int num = rand()%(110 - cant) + 1;
+  printf("%d\n", num);
+  DIR *dir_actual;
+  struct dirent *file;
+  dir_actual = opendir("./mazo");
+  if (dir_actual) {
+    while ((file = readdir(dir_actual)) != NULL) {
+      if (i==num)
+      {
+        if (strcmp(file->d_name,".")==0 || strcmp(file->d_name,"..")==0)
+          return obtenerCarta(cant);
+        printf("%s\n", file->d_name);
+        strcpy(carta,file->d_name);
+        printf("%s\n", carta);
+        printf("%s\n", getcwd(s, 100));
+        chdir("mazo");
+        remove(carta);
+        chdir("..");
+        break;
+      }
+      i++;
+    }
+    closedir(dir_actual);
+  } else {
+      printf("Error al abrir directorioo\n");
+      exit(0);
+  }
+  return carta;
+}
+
+void repartirCartas(){
+  char *carta;
+  for (int i = 0; i < 7; i++) {
+    carta = obtenerCarta((i*4));
+    entregarCarta("./player1",carta);
+    free(carta);
+    carta = obtenerCarta((i*4)+1);
+    entregarCarta("./player2",carta);
+    free(carta);
+    carta = obtenerCarta((i*4)+2);
+    entregarCarta("./player3",carta);
+    free(carta);
+    carta = obtenerCarta((i*4)+3);
+    entregarCarta("./player4",carta);
+    free(carta);
+  }
+  carta = obtenerCarta(28);
+  entregarCarta("./lastCard",carta);
+  free(carta);
+}
 
 void listFiles(char *dir){
     DIR *dir_actual;
@@ -12,9 +96,11 @@ void listFiles(char *dir){
     if (dir_actual) {
       while ((file = readdir(dir_actual)) != NULL) {
         printf("%s\n", file->d_name);
-        printf("%ld\n", telldir(dir_actual));
       }
       closedir(dir_actual);
+    } else {
+        printf("Error al abrir directorio\n");
+        exit(0);
     }
 }
 
@@ -32,49 +118,60 @@ void createCards(char *name, char *dir){
         carta = fopen(nombre,"w");
         fclose(carta);
     }
+  chdir("..");
   fclose(fp);
 }
 
 
 int main() {
-  char *name = "cartas.txt";
+srand(time(0));
+char *name = "cartas.txt";
 struct stat st = {0};
-//Create cards
-if (stat("./mazo",&st) < 0){
-  //Create directory
-  if (mkdir("./mazo", 0777) == -1){
+//Create directory
+if (stat("./mazo",&st) < 0) {
+  if (mkdir("./mazo", 0777) == -1)
     printf("Error para crear directorio mazo\n");
-  } else{
+}
+//Create cards
+if (stat("./mazo",&st) >= 0){
     char s[100];
     createCards(name,"mazo");
-    printf(" %s\n", getcwd(s, 100));
-    chdir("..");
-  }
+    printf(" %s\n", getcwd(s  , 100));
 }
-
 //Create players
 if (stat("./player1",&st) < 0) {
   if (mkdir("./player1", 0777) == -1)
     printf("Error para crear directorio player1\n");
+} else {
+  eliminarCartas("./player1");
 }
 if (stat("./player2",&st) < 0) {
   if (mkdir("./player2", 0777) == -1)
     printf("Error para crear directorio player2\n");
+} else {
+  eliminarCartas("./player2");
 }
 if (stat("./player3",&st) < 0) {
   if (mkdir("./player3", 0777) == -1)
     printf("Error para crear directorio player3\n");
+} else {
+  eliminarCartas("./player3");
 }
 if (stat("./player4",&st) < 0) {
   if (mkdir("./player4", 0777) == -1)
     printf("Error para crear directorio player4\n");
+} else {
+  eliminarCartas("./player4");
 }
 
 //Create lastCard
 if (stat("./lastCard",&st) < 0) {
   if (mkdir("./lastCard", 0777) == -1)
     printf("Error para crear directorio lastCard\n");
+} else {
+  eliminarCartas("./lastCard");
 }
-listFiles("./mazo");
-  return 0;
+//listFiles("./mazo");
+repartirCartas();
+return 0;
 }
